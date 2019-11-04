2a7d459ea0f4a68a1812fb18a2c8f7f8ccb2edfe
AlessandroPilleri <39701974+AlessandroPilleri@users.noreply.github.com>
2019-11-04 23:53:06 +0100

    Add animation to site start

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,25 @@
 var controller = new ScrollMagic.Controller();
+var timelineInit = new TimelineMax();
 
-const background = document.querySelector(".bcg-parallax");
+timelineInit.from(".round", 0.6, {
+  top: 0,
+  opacity: 0
+})
+.to(".round", 0.8, {
+  width: "20rem"
+})
+.from(".bcg-parallax img", 2, {
+  opacity: 0
+})
+.from(".title", 2, {
+  opacity: 0,
+  bottom: "40%"
+}, "-=2")
+.from("ul", 2, {
+  opacity: 0,
+  right: "-10%"
+}, "-=1.5")
+
 const range = 30;
 
 // const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2)).toFixed(1);
